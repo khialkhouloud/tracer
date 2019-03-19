593272aa93169f1fdf85c40c3011225b0ab50c0f
khialkhouloud <bk_khial@esi.dz>
2019-03-19 08:29:42 -0700

    update devdependencies with new version

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 "use strict";
+var test = require('tape');//var t = require("t");
 
-var assert = require("assert");
-exports["simple"] = function() {
+test("simple", function(t) {
 	var logger = require('../').console({
 		transport : function(data) {
 			console.log(data.output);
@@ -9,13 +9,14 @@ exports["simple"] = function() {
 		}
 	});
 	var o = logger.info('hello');
-	assert.equal(o['message'], 'hello');
-	assert.equal(o['file'], 'test.js');
-	assert.equal(o['line'], 11);
-	assert.equal(o['level'], 3);
-}
+	t.equal(o['message'], 'hello');
+	t.equal(o['file'], 'test.js');
+	t.equal(o['line'], '11');
+	t.equal(o['level'], 3);
+	t.end();
+})
 
-exports["stack index"] = function() {
+test("stack index",  function(t) {
 	var logger = require('../').console({
 		stackIndex: 1,
 		transport : function(data) {
@@ -27,12 +28,13 @@ exports["stack index"] = function() {
 		return logger[type](msg);
 	};
 	var o = logMgr('info', 'hello');
-	assert.equal(o['message'], 'hello');
-	assert.equal(o['file'], 'test.js');
-	assert.equal(o['line'], 29);
-}
+	t.equal(o['message'], 'hello');
+	t.equal(o['file'], 'test.js');
+	t.equal(o['line'], '30');
+	t.end();
+})
 
-exports["simple message"] = function() {
+test("simple message", function(t) {
 	var logger = require('../').console({
 		format : "{{message}}",
 		transport : function(data) {
@@ -41,10 +43,11 @@ exports["simple message"] = function() {
 		}
 	});
 	var o = logger.log('hello');
-	assert.equal(o['output'], 'hello');
-}
+	t.equal(o['output'], 'hello');
+	t.end();
+})
 
-exports["inspect depth"] = function() {
+test("inspect depth" ,function(t) {
 	var logger = require('../').console({
 		format : "{{message}}",
 		transport : function(data) {
@@ -65,10 +68,11 @@ exports["inspect depth"] = function() {
 			}
 		}
 	});
-	assert.equal(o['output'], "{ i1: 'value', i2: { i21: 'val21', i22: [Object] } }");
-}
+	t.equal(o['output'], "{ i1: 'value', i2: { i21: 'val21', i22: [Object] } }");
+	t.end();
+})
 
-exports["simple color message"] = function() {
+test("simple color message", function(t) {
 	var logger = require('../').colorConsole({
 		format : "{{message}}",
 		transport : function(data) {
@@ -77,10 +81,11 @@ exports["simple color message"] = function() {
 		}
 	});
 	var o = logger.debug('hello');
-	assert.equal(o.output, '\u001b[36mhello\u001b[39m');
-}
+	t.equal(o.output, '\u001b[36mhello\u001b[39m');
+	t.end();
+})
 
-exports["console log method"] = function() {
+test("console log method", function(t) {
 	var logger = require('../').console({
 		format : "{{message}}",
 		transport : function(data) {
@@ -89,12 +94,13 @@ exports["console log method"] = function() {
 		}
 	});
 	var o = logger.log('hello %s %d %j %t', 'world', 123, {j:'val'}, {t:'val'});
-	assert.equal(o['title'], 'log');
-	assert.equal(o['file'], '');//the format don't include "file", so can't get it
-	assert.equal(o['output'], 'hello world 123 {"j":"val"} { t: \'val\' }');
-}
+	t.equal(o['title'], 'log');
+	t.equal(o['file'], '');//the format don't include "file", so can't get it
+	t.equal(o['output'], 'hello world 123 {"j":"val"} { t: \'val\' }');
+	t.end();
+})
 
-exports["custom format"] = function() {
+test("custom format", function(t) {
 	var logger = require('../').console({
 		format : [
 		          "{{message}}", // default format
@@ -109,14 +115,15 @@ exports["custom format"] = function() {
 		}
 	});
 	var o = logger.log('hello %s %d', 'world', 123);
-	assert.equal(o['output'], 'hello world 123');
+	t.equal(o['output'], 'hello world 123');
 	o = logger.warn('hello %s %d', 'world', 123);
-	assert.equal(o['output'], 'warn:hello world 123');
+	t.equal(o['output'], 'warn:hello world 123');
 	o = logger.error('hello %s %d', 'world', 123);
-	assert.equal(o['output'], 'error:hello world 123');
-}
+	t.equal(o['output'], 'error:hello world 123');
+	t.end();
+})
 
-exports["custom filter"] = function() {
+test("custom filter",function(t) {
 	var colors = require('colors');
 	var logger = require('../').console({
 		format : [
@@ -138,96 +145,103 @@ exports["custom filter"] = function() {
 		}
 	});
 	var o = logger.log('hello %s %d', 'world', 123);
-	assert.equal(o['output'], '\u001b[4mhello world 123\u001b[24m');
-	assert.equal(o['level'], 0);
+	t.equal(o['output'], '\u001b[4mhello world 123\u001b[24m');
+	t.equal(o['level'], 0);
 	o = logger.warn('hello %s %d', 'world', 123);
-	assert.equal(o['output'], '\u001b[33mwarn:hello world 123\u001b[39m');
-	assert.equal(o['level'], 4);
+	t.equal(o['output'], '\u001b[33mwarn:hello world 123\u001b[39m');
+	t.equal(o['level'], 4);
 	o = logger.error('hello %s %d', 'world', 123);
-	assert.equal(o['output'], '\u001b[1m\u001b[31merror:hello world 123\u001b[39m\u001b[22m');
-	assert.equal(o['level'], 5);
-}
+	t.equal(o['output'], '\u001b[1m\u001b[31merror:hello world 123\u001b[39m\u001b[22m');
+	t.equal(o['level'], 5);
+	t.end();
+})
 
-exports["set level to log"] = function() {
+test("set level to log", function(t) {
 	var logger = require('../').console({level:'log',
 		transport : function(data) {
 			return data;
 		}
 	});
-	assert.ok(logger.log('hello'));
-	assert.ok(logger.trace('hello', 'world'));
-	assert.ok(logger.debug('hello %s',  'world', 123));
-	assert.ok(logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
-	assert.ok(logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
-	assert.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
-}
-exports["set level to 0"] = function() {
+	t.ok(logger.log('hello'));
+	t.ok(logger.trace('hello', 'world'));
+	t.ok(logger.debug('hello %s',  'world', 123));
+	t.ok(logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
+	t.ok(logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
+	t.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
+	t.end();
+})
+test("set level to 0", function(t) {
 	var logger = require('../').console({level:0,
 		transport : function(data) {
 			return data;
 		}
 	});
-	assert.ok(logger.log('hello'));
-	assert.ok(logger.trace('hello', 'world'));
-	assert.ok(logger.debug('hello %s',  'world', 123));
-	assert.ok(logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
-	assert.ok(logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
-	assert.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
-}
-exports["set level to 2"] = function() {
+	t.ok(logger.log('hello'));
+	t.ok(logger.trace('hello', 'world'));
+	t.ok(logger.debug('hello %s',  'world', 123));
+	t.ok(logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
+	t.ok(logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
+	t.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
+	t.end();
+})
+test("set level to 2", function(t) {
 	var logger = require('../').console({level:2,
 		transport : function(data) {
 			return data;
 		}
 	});
-	assert.ok(!logger.log('hello'));
-	assert.ok(!logger.trace('hello', 'world'));
-	assert.ok(logger.debug('hello %s',  'world', 123));
-	assert.ok(logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
-	assert.ok(logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
-	assert.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
-}
-exports["set level to warn"] = function() {
+	t.ok(!logger.log('hello'));
+	t.ok(!logger.trace('hello', 'world'));
+	t.ok(logger.debug('hello %s',  'world', 123));
+	t.ok(logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
+	t.ok(logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
+	t.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
+	t.end();
+})
+test("set level to warn",function(t) {
 	var logger = require('../').console({level:'warn',
 		transport : function(data) {
 			return data;
 		}
 	});
-	assert.ok(!logger.log('hello'));
-	assert.ok(!logger.trace('hello', 'world'));
-	assert.ok(!logger.debug('hello %s',  'world', 123));
-	assert.ok(!logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
-	assert.ok(logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
-	assert.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
-}
-exports["set level to error"] = function() {
+	t.ok(!logger.log('hello'));
+	t.ok(!logger.trace('hello', 'world'));
+	t.ok(!logger.debug('hello %s',  'world', 123));
+	t.ok(!logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
+	t.ok(logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
+	t.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
+	t.end();
+})
+test("set level to error", function(t) {
 	var logger = require('../').console({level:'error',
 		transport : function(data) {
 			return data;
 		}
 	});
-	assert.ok(!logger.log('hello'));
-	assert.ok(!logger.trace('hello', 'world'));
-	assert.ok(!logger.debug('hello %s',  'world', 123));
-	assert.ok(!logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
-	assert.ok(!logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
-	assert.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
-}
-exports["set level to max value"] = function() {
+	t.ok(!logger.log('hello'));
+	t.ok(!logger.trace('hello', 'world'));
+	t.ok(!logger.debug('hello %s',  'world', 123));
+	t.ok(!logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
+	t.ok(!logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
+	t.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
+	t.end();
+})
+test("set level to max value", function(t) {
 	var logger = require('../').console({level:Number.MAX_VALUE,
 		transport : function(data) {
 			return data;
 		}
 	});
-	assert.ok(!logger.log('hello'));
-	assert.ok(!logger.trace('hello', 'world'));
-	assert.ok(!logger.debug('hello %s',  'world', 123));
-	assert.ok(!logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
-	assert.ok(!logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
-	assert.ok(!logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
-}
+	t.ok(!logger.log('hello'));
+	t.ok(!logger.trace('hello', 'world'));
+	t.ok(!logger.debug('hello %s',  'world', 123));
+	t.ok(!logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
+	t.ok(!logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
+	t.ok(!logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
+	t.end();
+})
 
-exports["loop"] = function() {
+test("loop", function(t) {
 	var logger = require('../').console({
 		transport : function(data) {
 			console.log(data.output);
@@ -236,14 +250,16 @@ exports["loop"] = function() {
 	});
 	for(var i=0; i<100; i++){
 		var o = logger.info('hello');
-		assert.equal(o['message'], 'hello');
-		assert.equal(o['file'], 'test.js');
-		assert.equal(o['line'], 238);
-		assert.equal(o['level'], 3);
-	}
-}
+		t.equal(o['message'], 'hello');
+		t.equal(o['file'], 'test.js');
+		t.equal(o['line'], '252');
+		t.equal(o['level'], 3);
 
-exports["setLevel 1"] = function() {
+	}
+	t.end();
+})
+
+test("setLevel 1", function(t) {
 	var logger = require('../').console({
 		transport : function(data) {
 			console.log(data.output);
@@ -251,16 +267,17 @@ exports["setLevel 1"] = function() {
 		}
 	});
 	require('../').setLevel(2);
-	assert.ok(!logger.log('hello'));
-	assert.ok(!logger.trace('hello', 'world'));
-	assert.ok(logger.debug('hello %s',  'world', 123));
-	assert.ok(logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
-	assert.ok(logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
-	assert.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
+	t.ok(!logger.log('hello'));
+	t.ok(!logger.trace('hello', 'world'));
+	t.ok(logger.debug('hello %s',  'world', 123));
+	t.ok(logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
+	t.ok(logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
+	t.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
+	t.end();
 
-}
+})
 
-exports["setLevel 2"] = function() {
+test("setLevel 2", function(t) {
 	var logger = require('../').console({
 		transport : function(data) {
 			console.log(data.output);
@@ -268,16 +285,17 @@ exports["setLevel 2"] = function() {
 		}
 	});
 	require('../').setLevel('debug');
-	assert.ok(!logger.log('hello'));
-	assert.ok(!logger.trace('hello', 'world'));
-	assert.ok(logger.debug('hello %s',  'world', 123));
-	assert.ok(logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
-	assert.ok(logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
-	assert.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
+	t.ok(!logger.log('hello'));
+	t.ok(!logger.trace('hello', 'world'));
+	t.ok(logger.debug('hello %s',  'world', 123));
+	t.ok(logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
+	t.ok(logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
+	t.ok(logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
+  t.end();
+})
 
-}
 
-exports["close"] = function() {
+test("close", function(t) {
 	var logger = require('../').console({
 		transport : function(data) {
 			console.log(data.output);
@@ -285,21 +303,12 @@ exports["close"] = function() {
 		}
 	});
 	require('../').close();
-	assert.ok(!logger.log('hello'));
-	assert.ok(!logger.trace('hello', 'world'));
-	assert.ok(!logger.debug('hello %s',  'world', 123));
-	assert.ok(!logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
-	assert.ok(!logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
-	assert.ok(!logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
-	assert.ok(!logger.fatal('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object, logger));
-
-}
-
-exports["simple"] = function() {
-	var logger = require('../').console();
-	var o = logger.log('hello');
-	assert.equal(o['message'], 'hello');
-	assert.equal(o['file'], 'test.js');
-	assert.equal(o['line'], 300);
-	assert.equal(o['level'], 0);
-}
+	t.ok(!logger.log('hello'));
+	t.ok(!logger.trace('hello', 'world'));
+	t.ok(!logger.debug('hello %s',  'world', 123));
+	t.ok(!logger.info('hello %s %d',  'world', 123, {foo:'bar'}));
+	t.ok(!logger.warn('hello %s %d %j', 'world', 123, {foo:'bar'}));
+	t.ok(!logger.error('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object));
+	t.ok(!logger.fatal('hello %s %d %j', 'world', 123, {foo:'bar'}, [1, 2, 3, 4], Object, logger));
+  t.end();
+})
